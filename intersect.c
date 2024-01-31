a55c51bb4ea705c2938a695a0dd449988eccbcea
dellmonitor <123smorodina@gmail.com>
2024-01-31 14:14:49 +0300

    replace power_of_two function with left shift operator

--- intersect.c
+++ intersect.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 
-unsigned int power_of_two(int i)
-{
-	unsigned int value = 1;
-	while (i > 0) {
-		value = value << 1;
-		i = i - 1;
-	}
-	return value;
-}
-
-
 int main(int argc, char **argv)
 {
 	int cardA, cardB, i, a_number, b_number;
@@ -18,12 +7,12 @@ int main(int argc, char **argv)
 	scanf("%d", &cardA);
 	for (i = 0; i < cardA; i++) {
 		scanf("%d", &a_number);
-		A = A + power_of_two(a_number);
+		A = A + (1 << a_number);
 	}
 	scanf("%d", &cardB);
 	for (i = 0; i < cardB; i++) {
 		scanf("%d", &b_number);
-		B = B + power_of_two(b_number);
+		B = B + (1 << b_number);
 	}
 	intersect = A & B;
 	for (i = 0; i < 32; i++) {
