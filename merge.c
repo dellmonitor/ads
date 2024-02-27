59a4cbb733942a5b758e3434cd24148814671baf
dellmonitor <123smorodina@gmail.com>
2024-02-28 02:49:57 +0300

    add merge.c

---
+++ merge.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct PriorityQueue {
+	int *heap;
+	int cap, count;
+};
+
+int main(int argc, char **argv)
+{
+	int k, i, n;
+	scanf("%d", &k);
+	sizes[k];
+	for (i = 0; i < k; i++) {
+		scanf("%d", sizes[i]);
+		n = n + i;
+	}
+	struct PriorityQueue q = InitPriorityQueue(n);
+
+	return 0;
+}
